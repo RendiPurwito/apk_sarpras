34a1227e3ed365cfa06bb514dc41fa96d4deb069
faiz1701 <thefaiz17@gmail.com>
2022-06-14 14:36:06 +0700

    2

---
+++ database/migrations/2022_06_14_070829_add_trigger.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared(" CREATE TRIGGER stok_masuk AFTER INSERT ON 'barang_masuks' FOR EACH ROW BEGIN
+        INSERT INTO 'barangs' ('') VALUES (NEW.id) ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
